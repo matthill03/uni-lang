796cb83653e957344cf4e14d04d9c586bf27bafd
Matthew Hill <contact@matthew-hill.net>
2025-02-17 14:42:46 +0000

    Add file pass in through command line

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
+import sys
+
 from lexer import lex
 from parser import parse
 
+def print_usage():
+    print(f"Usage: python3 src/main.py <file_path>")
+
 if __name__ == "__main__":
-    tokens = lex("examples/boolean.qk")
+    if len(sys.argv) < 2:
+        print_usage()
+        exit(1)
+
+    tokens = lex(sys.argv[1])
     ast = parse(tokens)
